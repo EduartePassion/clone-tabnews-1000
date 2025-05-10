772288519c490c6dd5263a6d5d0793aae90afbd7
edilson <59458835+EduartePassion@users.noreply.github.com>
2025-05-10 20:47:15 +0000

    ci: add commitlint with Conventional Commits config
    ci: add commitlint with Conventional Commits config
    
    commitlint.config.js package-lock.json
    
    .editorconfig
    
    commitlint.config.js
    
    modified:commitlint.config.js

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "ci",
+        "build",
+        "revert",
+      ],
+    ],
+  },
+};
